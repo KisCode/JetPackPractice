2cf9cdde8b39f890d8ed38821c4a0e2143a0166d
kisCode <keno123@126.com>
2021-11-15 17:02:20 +0800

    加入了Hilt示例:单例线程池

--- hilt_sample/src/main/java/kiscode/jetpack/hilt/HomeActivity.java
+++ hilt_sample/src/main/java/kiscode/jetpack/hilt/HomeActivity.java
@@ -2,9 +2,13 @@ package kiscode.jetpack.hilt;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.ExecutorService;
 
 import javax.inject.Inject;
 
@@ -37,10 +41,14 @@ public class HomeActivity extends AppCompatActivity {
     @MakeBlueCat
     Cat mBlueCat;
 
+    @Inject
+    ExecutorService mExecutor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initView();
 
         TextView tvName = findViewById(R.id.tv_name);
         tvName.setText("name is " + mUser.name);
@@ -53,5 +61,22 @@ public class HomeActivity extends AppCompatActivity {
         Log.i(TAG, "cat name is " + mBlueCat.getName());
         Log.i(TAG, "cat name is " + mRedCat.getName());
 
+    }
+
+    private void initView() {
+        Button btnExecute = findViewById(R.id.btn_execute);
+        btnExecute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 10000; i++) {
+                    mExecutor.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.i(TAG, Thread.currentThread().getName());
+                        }
+                    });
+                }
+            }
+        });
     }
 }
